91f97be2c4542720af5571b84e4cef5bbb1ad39e
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-17 03:41:24 +0300

    html for the book appointment page

--- index/book_appointment.php
+++ index/book_appointment.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/book_appointment.css"  rel ="stylesheet"></link>
+ 
     <title>Book Appointment</title>
 </head>
 <body>
-    
+   <h1 class="heading"> <span>book</span> now </h1>
+   <div class="row">
+    <div class="image">
+        <img src="../images/waiting_room.jpg" alt = "">
+</div>
+<form action="">
+    <h3 > book appointment </h3>
+    <input type ="text" placeholder="your name" class="box">
+    <input type ="number" placeholder="your number" class="box">
+    <input type ="email" placeholder="your email" class="box">
+    <input type ="date"  class="box">
+    <input type ="submit" placeholder="book now" class="btn">
+
+</form> 
+
 </body>
 </html>
