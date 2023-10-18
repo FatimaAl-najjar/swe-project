19934d8d8f52dbbc87dfdeccbf5fbdf2f3de477e
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-18 22:52:27 +0300

    try again

--- index/try.php
+++ index/try.php
@@ -34,15 +34,43 @@
             width: 70px;
             /* Add any specific styling for your logo image */
         }
+        .logo-scroll {
+            height: 50px;
+            width: 50px;
+            /* Add any specific styling for your scrolled logo image */
+        }
     </style>
+    <script>
+        window.addEventListener('scroll', function() {
+            var logo = document.querySelector(".logo-image");
+            if (window.scrollY > 0) { // Change the value as per your requirement for when the scroll should trigger the logo change
+                logo.classList.add("logo-scroll");
+            } else {
+                logo.classList.remove("logo-scroll");
+            }
+        });
+    </script>
 </head>
 <body>
     <ul class="navbar">
-        <li><a href="#"><img class="logo-image" src="../images/L1.png" alt="Logo1" ></a></li>
+        <li><a href="#"><img class="logo-image" src="../images/L1.png" alt="Logo1"></a></li>
         <li><a href="#">About Us</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Login</a></li>
         <li><a href="#">Sign Up</a></li>
     </ul>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <img src="../images/L1.png" alt="Logo1">
+    <br>
+    <!-- Add your content here -->
 </body>
 </html>
