9fb3e2c5853f0e472a91bcb2b475b6d27d9b6af2
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-18 20:50:09 +0300

    Push

--- index/signup.php
+++ index/signup.php
@@ -2,45 +2,44 @@
 include_once "../includes/dbh.inc.php";
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign up</title>
-</head>
-<body>
-    <form action="" method="post">
-    <label>FirstName:</label>
-    <input type="text" name="FirstName" placeholder="please enter your firstname"><br>
-    <label>LastName:</label>
-    <input type="text" name="LastName" placeholder="please enter your lastname"><br>
-    <label>Email:</label>
-    <input type="text" name="Email" placeholder="please enter your email"><br>
-    <label>password:</label>
-    <input type="password" name="Password" placeholder="please enter your password:"><br>
-    <label>re-enter password</label>
-    <input type="password" name="repeatPassword" placeholder="please re-enter your Password" required><br>
-    <label>Phone number:</label>
-    <input type="number" name="Phonenumber" placeholder="please enter your number"><br>
-    <button type="submit" value="submit">Sign up</button>
-    </form>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sign up</title>
+    </head>
+    <body>
+        <form action="" method="post">
+        <label>FirstName:</label>
+        <input type="text" name="FirstName" placeholder="please enter your firstname"><br>
+        <label>LastName:</label>
+        <input type="text" name="LastName" placeholder="please enter your lastname"><br>
+        <label>Email:</label>
+        <input type="text" name="Email" placeholder="please enter your email"><br>
+        <label>password:</label>
+        <input type="password" name="Password" placeholder="please enter your password:"><br>
+        <label>re-enter password</label>
+        <input type="password" name="repeatPassword" placeholder="please re-enter your Password" required><br>
+        <label>Phone number:</label>
+        <input type="number" name="Phonenumber" placeholder="please enter your number"><br>
+        <button type="submit" value="submit">Sign up</button>
+        </form>
 
-    <?php
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $FirstName = htmlspecialchars($_POST["FirstName"]);
-        $LastName = htmlspecialchars($_POST["LastName"]);
-        $Email = htmlspecialchars($_POST["Email"]);
-        $Password = htmlspecialchars($_POST["Password"]);
-        $Phonenumber = htmlspecialchars($_POST["Phonenumber"]);
-        $sql = "insert into patients(FirstName,LastName,Email,Password,Phonenumber) 
-        values('$FirstName','$LastName','$Email','$Password','$Phonenumber')";
-        $result = mysqli_query($conn, $sql);
+        <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $FirstName = htmlspecialchars($_POST["FirstName"]);
+            $LastName = htmlspecialchars($_POST["LastName"]);
+            $Email = htmlspecialchars($_POST["Email"]);
+            $Password = htmlspecialchars($_POST["Password"]);
+            $Phonenumber = htmlspecialchars($_POST["Phonenumber"]);
+            $sql = "insert into patients(FirstName,LastName,Email,Password,Phonenumber) 
+            values('$FirstName','$LastName','$Email','$Password','$Phonenumber')";
+            $result = mysqli_query($conn, $sql);
 
-        if($result){
-            header("location:login.php");
+            if($result){
+                header("location:login.php");
+            }
         }
-    }
-    ?>
-</body>
+        ?>
+    </body>
 </html>
