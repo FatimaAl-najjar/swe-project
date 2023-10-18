95d48a483d205acecc1a07d667e59ae50106bd40
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-18 23:31:41 +0300

    zht :)

--- index/try.php
+++ index/try.php
@@ -3,18 +3,16 @@
 <head>
     <title>Nav bar</title>
     <style>
-        /* Add any desired styling to your navigation bar */
-        /* For example: */
         .navbar {
             position: fixed;
-            top: 0;
+            top: 0px;
             width: 100%;
             z-index: 999; /* Adjust the z-index value as per your needs */
             list-style-type: none;
-            margin: 0;
-            padding: 0;
+            margin: 0px;
+            padding: 0px;
             overflow: hidden;
-            background-color: #f1f1f1;
+            background-color: #fff;
         }
 
         .navbar li {
@@ -23,55 +21,69 @@
 
         .navbar li a {
             display: block;
-            color: #333;
+            color: #484da2;
             text-align: center;
-            padding: 14px 16px;
+            padding: 20px 20px;
             text-decoration: none;
         }
 
-        .navbar li a:hover {
-            background-color: #ddd;
-        }
+        .navbar li a {
+    position: relative; /* Add relative positioning to the anchor element */
+}
 
+.navbar li a::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 2px;
+    background-color: #02309b; /* Set the color of the horizontal line */
+    transition: width 0.3s ease-out; /* Add a transition effect for smooth animation */
+}
+
+.navbar li a:hover::after {
+    width: 100%; /* Expand the width of the line when hovering */
+}
         .logo-image {
-            height: 70px;
-            width: 70px;
-            /* Add any specific styling for your logo image */
+            height: 80px;
+            width: 300px;
         }
         .logo-scroll {
-            height: 50px;
-            width: 50px;
-            /* Add any specific styling for your scrolled logo image */
+            height: 70px;
+            width: 85px;
         }
-        /* Add styling to push the content down to make space for the fixed navigation bar */
+       
         body {
-            margin-top: 70px; /* Adjust the value to match the height of the navigation bar */
+            padding:50px;
+            margin-top: 70px; 
         }
     </style>
     <script>
         window.addEventListener('scroll', function() {
             var logo = document.querySelector(".logo-image");
-            if (window.scrollY > 0) { // Change the value as per your requirement for when the scroll should trigger the logo change
+            if (window.scrollY > 0) { 
                 logo.classList.add("logo-scroll");
-                logo.src = "../images/logo3.png"; // Path to the second logo image
-                logo.alt = "Logo2"; // Alternate text for the second logo image
+                logo.src = "../images/L1.png"; 
+                logo.alt = "Logo2"; 
             } else {
                 logo.classList.remove("logo-scroll");
-                logo.src = "../images/L1.png"; // Path to the original logo image
-                logo.alt = "Logo1"; // Alternate text for the original logo image
+                logo.src = "../images/logo3.png"; 
+                logo.alt = "Logo1"; 
             }
         });
     </script>
 </head>
 <body>
     <ul class="navbar">
-        <li><a href="#"><img class="logo-image" src="../images/L1.png" alt="Logo1"></a></li>
+        <li><img class="logo-image" src="../images/logo3.png" alt="Logo1"></li>
         <li><a href="#">About Us</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Login</a></li>
         <li><a href="#">Sign Up</a></li>
     </ul>
-    <img src="../images/L1.png" alt="Logo1">
+    <!-- test -->
+    
     <br>
     <img src="../images/L1.png" alt="Logo1">
     <br>
@@ -83,6 +95,6 @@
     <br>
     <img src="../images/L1.png" alt="Logo1">
     <br>
-    <!-- Add your content here -->
+    
 </body>
 </html>
