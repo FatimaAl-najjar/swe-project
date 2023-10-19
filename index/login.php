c46fa37433c8c5741d8cb251b053abd315afa36a
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-19 22:08:40 +0300

    LINK

--- index/login.php
+++ index/login.php
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION["Phonenumber"] = $row["Phonenumber"];
 
         // Redirect after a successful login
-        header("Location: ../signup.php");
+        header("Location:signup.php");
         exit;
     }
 }
