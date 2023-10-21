9421993d0de49540969270d2f3a3554331211be3
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-21 17:44:15 +0300

    Adding navbar and footer

--- index/feedback.php
+++ index/feedback.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-// include '../includes/nav.php';
-// include_once "../includes/dbh.inc.php";
+include '../includes/nav.php';
+// include_once "../includes/dbh.inc.php";  // I made it from scratch line 22
 ?> 
 
 <!doctype html>
@@ -51,9 +51,11 @@ session_start();
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <a href="homePage.php"><button type="button" class="btn btn-primary">Back</button></a>
             </form>
-            
         </div>
+        <br>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
+
+<footer><?php include '../includes/footer.php';?></footer>
