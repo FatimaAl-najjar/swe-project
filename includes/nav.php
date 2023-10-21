e2b763f7ccb7bdbce019a8a36fa21e5a9e89743e
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-21 23:53:58 +0300

    el nav malbosa ya gama3a

--- includes/nav.php
+++ includes/nav.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/nav.css" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css">
-   
     <script>
         window.addEventListener("scroll", function() {
             var logo = document.querySelector(".logo-image");
--- index/book_appointment.php
+++ index/book_appointment.php
@@ -4,66 +4,7 @@ include '../includes/nav2.php';
  
 // include_once "/includes/login.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- ... your HTML head content ... -->
-    
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/book_appointment.css">
-        <title>Document</title>
-   
-        
-   
-</head>
-<body>
-   <!-- ... your HTML content ... -->
-   <div class="row"> 
-   <div class="home-img">
-    <img class="my-img" src="../images/waiting_room2.jpg">
-   </div>
-   <form action="" method="post">
-       <!-- ... your form inputs ... -->
-       <input type="time" name="time" class="box">
-       <input type="date" name="date" class="box">
-       <input type="submit" name="book_now" class="btn">
-        
-       </form>
-   </div>
-
- 
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ensure that the user is logged in
-    if (isset($_SESSION['ID'])) {
-        $user_id = $_SESSION['ID'];
-
-        $Time = $_POST["time"]; // Modify to match your database column name
-        $Date = $_POST["date"];
-
-        // Insert the appointment into the database along with the user's ID
-        $sql = "INSERT INTO timeslots (date, duration, patients_id) 
-                VALUES ('$Date', '$Time', '$user_id')";
-
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            header("Location:../index/signup.php");
-            exit;
-        }
-    } else {
-        // Handle the case when the user is not logged in
-        echo "You must be logged in to book an appointment.";
-    }
-}
-?>
- 
- </body>
-</html>
-<footer>
-    <?php include '../includes/footer.php';?>
-</footer>
+ <img src="../images/L1.png" alt="logo">
+ <br>
+ <img src="../images/L1.png" alt="logo">
+ <br>
