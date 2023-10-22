b155b92783a5dbbb93804389ab750842fc773045
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-22 03:01:13 +0300

    validations for the suitable dates of the clinic

--- index/book_appointment.php
+++ index/book_appointment.php
@@ -41,7 +41,7 @@ $halfHourLaterFormatted = $halfHourLater->format('Y-m-d\TH:i');
        <!-- <input type="time" name="time" class="box"> -->
        <input type="datetime-local" name="datetime" class="box" min="<?php echo $halfHourLaterFormatted; ?>">
        <!-- <input type="date" name="date" class="box"> -->
-       <input type="submit" name="book_now" class="btn">
+       <input type="submit" name="book_now" class="btn1">
         
        </form>
    </div>
@@ -58,20 +58,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $selectedDateTime = new DateTime($DateTime);
         $Date = $selectedDateTime->format('Y-m-d');
         $Time = $selectedDateTime->format('H:i:s');
+        // Check if the selected day is within Saturday to Thursday
+        $selectedDay = $selectedDateTime->format('N'); // 1 (Monday) to 7 (Sunday)
+        if ($selectedDay < 1) { // Not Saturday to Thursday
+            echo "<div class='error-box'>Appointments are only available from Saturday to Thursday.</div>";
+            exit;
+        }
 
+        // Check if the selected time is within 5pm to 10pm
+        $selectedTime = $selectedDateTime->format('H:i');
+        $startTime = new DateTime('17:00');
+        $endTime = new DateTime('22:00');
+        if ($selectedTime < $startTime->format('H:i') || $selectedTime > $endTime->format('H:i')) {
+            echo "<div class='error-box'>Appointments are only available from 5pm to 10pm.</div>";
+            exit;
+        }
         // Calculate the start and end time of the half-hour range
         $halfHourEarlier = clone $selectedDateTime;
         $halfHourEarlier->sub(new DateInterval('PT30M'));
         $startTime = $halfHourEarlier->format('H:i:s');
         $endTime = $Time;
+        // Delete any previous appointments for the same patient on the same day
+        $deleteSql = "DELETE FROM timeslots WHERE date = '$Date' AND patients_id = '$user_id'";
+        $deleteResult = mysqli_query($conn, $deleteSql);
 
+        if (!$deleteResult) {
+            echo "<div class='error-box'>Error deleting previous appointments.</div>";
+            exit;
+        }
         // Check if there are any existing appointments within the half-hour range
         $checkSql = "SELECT * FROM timeslots WHERE date = '$Date' AND duration >= '$startTime' AND duration <= '$endTime'";
         $checkResult = mysqli_query($conn, $checkSql);
 
         if (mysqli_num_rows($checkResult) > 0) {
             // There is already an appointment within the half-hour range
-            echo "There is already an appointment booked within the selected time range. Please choose a different time.";
+            echo "<div class='error-box'>There is already an appointment booked within the selected time range. Please choose a different time.</div>";
         }  else {
             // Insert the appointment into the database along with the user's ID
             $sql = "INSERT INTO timeslots (date, duration, patients_id) 
@@ -82,12 +103,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location:../index/book_appointment.php");
                 exit;
             } else {
-                echo "Error booking the appointment.";
+                echo "<div class='error-box'>Error booking the appointment.</div>";
             }
         }
     } else {
         // Handle the case when the user is not logged in
-        echo"You must be logged in to book an appointment.";
+        echo"<div class='error-box'>You must be logged in to book an appointment.</div>";
     }
 }
 ?>
