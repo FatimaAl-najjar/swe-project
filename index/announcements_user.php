55f78a2b93eefea102f4879637fbace76b30b490
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-23 22:34:26 +0300

    fixing the error of the time and basic css for the users announcements

--- index/announcements_user.php
+++ index/announcements_user.php
@@ -1,47 +1,28 @@
 <?php
-session_start();
 include_once "../includes/dbh.inc.php";
-// include '../includes/nav2.php';
+
+// Retrieve announcements from the database
+$sql = "SELECT * FROM announcements ORDER BY date_added DESC";
+$result = mysqli_query($conn, $sql);
+$announcements = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/announcements.css">
     <title>Announcements</title>
 </head>
 <body>
-<?php
-// Retrieve announcements from the database
-$sql = "SELECT announcement, date_added FROM announcements";
-$result = mysqli_query($conn, $sql);
+<h1>Announcements</h1>
 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $announcement = $row["announcement"];
-        $dateAdded = date("Y-m-d", strtotime($row["date_added"]));
-        $timeAdded = date("H:i:s", strtotime($row["date_added"]));
-
-        // Get the current date and time
-        $currentDate = date("Y-m-d");
-        $currentTime = date("H:i:s");
-
-        // Calculate the difference in days between the current date and the date the announcement was added
-        $daysDifference = floor((strtotime($currentDate) - strtotime($dateAdded)) / (60 * 60 * 24));
-
-        // Display the announcement if it has been less than or equal to 7 days (1 week)
-        if ($daysDifference <= 7) {
-            echo "<p><strong>Date:</strong> $dateAdded</p>";
-            echo "<p><strong>Time:</strong> $timeAdded</p>";
-            echo "<p><strong>Announcement:</strong> $announcement</p>";
-            echo "<hr>";
-        }
-    }
-}
-
-    else {
-        echo "<p>No announcements available.</p>";
-    }
-    ?>
+    <!-- Display announcements -->
+    <?php foreach ($announcements as $announcement): ?>
+        <div class="announcement-card">
+            <p class="date"><?php echo date("F j, Y H:i:s", strtotime($announcement['date_added'])); ?></p>
+            <p class="content"><?php echo $announcement['announcement']; ?></p>
+        </div>
+    <?php endforeach; ?>
 </body>
 </html>
