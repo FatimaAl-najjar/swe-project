4f31aaf03a13fd6e121f94e7b778dd552cd38f6b
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-23 21:02:55 +0300

    display the announcement to the user page

--- index/announcements_user.php
+++ index/announcements_user.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include_once "../includes/dbh.inc.php";
+  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,19 @@
     <title>Announcements</title>
 </head>
 <body>
-    
+<?php
+    // Retrieve announcements from the database
+    $sql = "SELECT announcement FROM announcements";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Output each announcement as a separate paragraph
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<p>" . $row["announcement"] . "</p>";
+        }
+    } else {
+        echo "<p>No announcements available.</p>";
+    }
+    ?>
 </body>
 </html>
