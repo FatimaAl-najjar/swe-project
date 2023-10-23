953bcb01b3285e567f922c4bd2e052cc713b29eb
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-23 21:47:56 +0300

    adding the date in the announcements table in db and fixing errors

--- admin/announcements.php
+++ admin/announcements.php
@@ -1,9 +1,20 @@
 <?php
 session_start();
 include_once "../includes/dbh.inc.php";
- 
-  
+
+if (isset($_POST['submit'])) {
+    $announcement = $_POST['announcement'];
+    $dateAdded = date("Y-m-d"); // Get the current date
+    
+    // Insert the announcement and current date into the table
+    $sql = "INSERT INTO announcements (announcement, date_added) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $announcement, $dateAdded);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,37 +23,11 @@ include_once "../includes/dbh.inc.php";
     <title>Announcements</title>
 </head>
 <body>
-<?php
- 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $announcement = $_POST["announcements"]; // Retrieve the announcement from the form
-
-    // Prepare the SQL statement with a prepared statement to prevent SQL injection
-    $sql = "INSERT INTO announcements (announcement) VALUES (?)";
-
-    $stmt = mysqli_stmt_init($conn);
-    if (mysqli_stmt_prepare($stmt, $sql)) {
-        mysqli_stmt_bind_param($stmt, "s", $announcement);
-        mysqli_stmt_execute($stmt);
-
-        // Redirect after a successful announcement
-        header("Location: announcements.php");
-        exit;
-    } else {
-        $errorMessage = "<h2>Failed to announce.</h2>";
-    }
-}
-?>
-    <form action="" method="post"> 
-    <label>please write here your announcement:</label>
-    <input type="text" name="announcements" placeholder="Enter your announcement" class="box">
-    <input type="submit" value="submit">
+    <!-- Form for adding new announcements -->
+    <form method="POST" action="">
+        <textarea name="announcement" placeholder="Enter announcement"></textarea>
+        <button type="submit" name="submit">Add Announcement</button>
     </form>
-    
-</body>
-
  
+ </body>
 </html>
-
- 
--- index/announcements_user.php
+++ index/announcements_user.php
@@ -12,31 +12,32 @@ include_once "../includes/dbh.inc.php";
 </head>
 <body>
 <?php
-    // Retrieve announcements from the database
-    $sql = "SELECT announcement FROM announcements";
-    $result = mysqli_query($conn, $sql);
+// Retrieve announcements from the database
+$sql = "SELECT announcement, date_added FROM announcements";
+$result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $announcement = $row["announcement"];
-            $dateAdded = date("Y-m-d", strtotime($row["date_added"]));
-            $timeAdded = date("H:i:s", strtotime($row["date_added"]));
-    
-            // Get the current date and time
-            $currentDate = date("Y-m-d");
-            $currentTime = date("H:i:s");
-    
-    // Calculate the difference in days between the current date and the date the announcement was added
-    $daysDifference = floor((strtotime($currentDate) - strtotime($dateAdded)) / (60 * 60 * 24));
-        }    
-         // Display the announcement if it has been less than or equal to 7 days (1 week)
-         if ($daysDifference <= 7) {
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $announcement = $row["announcement"];
+        $dateAdded = date("Y-m-d", strtotime($row["date_added"]));
+        $timeAdded = date("H:i:s", strtotime($row["date_added"]));
+
+        // Get the current date and time
+        $currentDate = date("Y-m-d");
+        $currentTime = date("H:i:s");
+
+        // Calculate the difference in days between the current date and the date the announcement was added
+        $daysDifference = floor((strtotime($currentDate) - strtotime($dateAdded)) / (60 * 60 * 24));
+
+        // Display the announcement if it has been less than or equal to 7 days (1 week)
+        if ($daysDifference <= 7) {
             echo "<p><strong>Date:</strong> $dateAdded</p>";
             echo "<p><strong>Time:</strong> $timeAdded</p>";
             echo "<p><strong>Announcement:</strong> $announcement</p>";
             echo "<hr>";
         }
     }
+}
 
     else {
         echo "<p>No announcements available.</p>";
