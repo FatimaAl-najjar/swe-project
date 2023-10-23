5ddc3febd3c4f0911cbef031da7f674b51bdc1f5
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-24 00:33:15 +0300

    yala

--- index/view_Profile.php
+++ index/view_Profile.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["Email"])) {
     $password = $_SESSION["Password"];
 } else {
     // Redirect to the login or signup page
-    header("Location: signup.php");
+    header("Location: homepage.php");
     exit();
 }
 ?> 
