826dbf0cf79e78ed4c64957f3da786edeb07a09e
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-10-24 00:28:07 +0300

    bnbzt el view profile

--- index/view_Profile.php
+++ index/view_Profile.php
@@ -2,6 +2,19 @@
 session_start();
 include '../includes/nav2.php';
 include_once "../includes/dbh.inc.php";
+
+// Check if the user is logged in or signed up
+if (isset($_SESSION["Email"])) {
+    // User is logged in or signed up
+    $firstName = $_SESSION["FirstName"];
+    $lastName = $_SESSION["LastName"];
+    $email = $_SESSION["Email"];
+    $password = $_SESSION["Password"];
+} else {
+    // Redirect to the login or signup page
+    header("Location: login.php");
+    exit();
+}
 ?> 
 
 <head>
@@ -22,13 +35,13 @@ include_once "../includes/dbh.inc.php";
                 <br><hr><br>
                 <div class="profile-info">
                     <p class="profile-label">User Name:</p>
-                    <p class="profile-value"><?php  echo $_SESSION["FirstName"] . " " . $_SESSION["LastName"]; ?></p>
+                    <p class="profile-value"><?php  echo $firstName . " " . $lastName; ?></p>
   
                     <p class="profile-label">Email:</p>
-                    <p class="profile-value"><?php echo $_SESSION["Email"]; ?></p>
+                    <p class="profile-value"><?php echo $email; ?></p>
                         
                     <p class="profile-label">Password:</p>
-                    <p class="profile-value"><?php echo $_SESSION["Password"]; ?></p>
+                    <p class="profile-value"><?php echo $password; ?></p>
                 </div>
             </form>
         </div>
