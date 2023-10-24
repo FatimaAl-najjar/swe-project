f3227248ca20361a52d1b8d22d6652a9f993846c
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-24 16:56:25 +0300

    Revert "2rft omi"
    This reverts commit ca0de9ee8d2ed6e64da1c8aca03c563732b94844.

--- index/view_Profile.php
+++ index/view_Profile.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["Email"])) {
     $password = $_SESSION["Password"];
 } else {
     // Redirect to the login or signup page
-    header("Location: view_Profile.php");
+    header("Location: homePage.php");
     exit();
 }
 ?> 
