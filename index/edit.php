0fe2e39c84416ca9efcd9cbed8410bac517fdadc
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-24 23:19:50 +0300

    kda kfaya elnahrada

--- index/edit.php
+++ index/edit.php
@@ -1,12 +1,66 @@
 <?php
 
 session_start();
-
+include '../includes/nav2.php';
 // Include connection
 include_once "../includes/dbh.inc.php";
 ?>
-<h1>Edit Profile</h1>
 
+<style>
+
+	
+.card {
+            background-color: #fff;
+            border-radius: 6px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 40px;
+        }
+        
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #484da0;
+        }
+
+        /* Full-width input fields */
+        input[type=text], input[type=password], input[type=number] {
+            width: 93%;
+            padding: 15px;
+            margin: 5px 0 22px 0;
+            display: inline-block;
+            border: none;
+            background: #f1f1f1;
+        }
+
+        input[type=text]:focus, input[type=password]:focus, input[type=number]:focus {
+            background-color: #8184c7;
+            outline: none;
+        }
+
+        label {
+            display: inline-block;
+            width: 150px;
+        }
+
+        .btn {
+            color: white;
+            padding: 15px 25px;
+            margin: 9px 0;
+            border: none;
+            /* cursor: pointer; */
+            width: 100%;
+			background-color: #484da0;
+            opacity: 0.9;
+        }
+
+        .btn:hover {
+            opacity: 1;
+        }
+
+</style>
+<br><br><br>
+<div class="card">
 <form action='' method='post'>
 	First Name:<br>
 	<input type='text' value="<?=$_SESSION['FirstName']?>" name='FirstName'><br>
@@ -18,9 +72,10 @@ include_once "../includes/dbh.inc.php";
 	<input type='text' value="<?=$_SESSION['Password']?>" name='Password'><br>
 	Phone number:<br>
 	<input type='text' value="<?=$_SESSION['Phonenumber']?>" name='Phonenumber'><br>
-	<input type='submit' value='Submit' name='Submit'>
+	<input class="btn" type='submit' value='Submit' name='Submit'>
+	<a href="homepage.php"><button type="button" class="btn">Back</button></a>
 </form>
-
+</div>
 
 <?php
 if($_SERVER['REQUEST_METHOD']== "POST"){ //check if form was submitted
@@ -40,7 +95,7 @@ if($_SERVER['REQUEST_METHOD']== "POST"){ //check if form was submitted
 		$_SESSION["Email"]=$Email;
 		$_SESSION["Password"]=$Password;
 		$_SESSION["Phonenumber"]=$Phonenumber;
-		header("Location:../index/view_profile.php");
+		header("Location:../index/edit.php");
 	}
 	else {
 		echo $sql;
--- index/view_Profile.php
+++ index/view_Profile.php
@@ -16,17 +16,65 @@ $sql = "SELECT * FROM patients WHERE ID = '$userId'";
 $result = mysqli_query($conn, $sql);
 $userInfo = mysqli_fetch_assoc($result);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/.css" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
     
     <title>View Profile</title>
+    <style>
+     .card {
+       border: 3px solid #484da0;
+        border-radius: 10px;
+        padding: 25px;
+        margin: 25px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .profile-info {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .profile-label {
+        font-weight: bold;
+        font-size:20px;
+        margin-bottom: 20px;
+    }
+
+    .profile-value {
+        margin-top: 5px;
+        font-size:20px;
+        margin-bottom: 20px;
+    }
+
+    a {
+        margin-right: 10px;
+        text-decoration: none;
+        color: blue;
+    }
+
+    .button-link {
+        display: inline-block;
+        padding: 15px 23px;
+        background-color: #484da0;
+        border: none;
+        color: #fff;
+        text-decoration: none;
+        font-size:20px;
+        cursor: pointer;
+        border-radius: 4px;
+    }
+
+    .button-link:hover {
+        background-color: #8b8ecb;
+    }
+    </style>
 </head>
 <body>
+    <br><br><br>
     <section class="home">
         <div class="form-container">
             <i class="uil uil-times form_close"></i>
@@ -36,16 +84,17 @@ $userInfo = mysqli_fetch_assoc($result);
                         <h2 style="text-align: center;">My Profile</h2>
                         <br><hr><br>
                         <div class="profile-info">
-                            <p class="profile-label">User Name:</p>
+                            <label for="username" class="profile-label">User Name:</label>
                             <p class="profile-value"><?php echo $userInfo["FirstName"] . " " . $userInfo["LastName"]; ?></p>
 
-                            <p class="profile-label">Email:</p>
+                            <label for="email" class="profile-label">Email:</label>
                             <p class="profile-value"><?php echo $userInfo["Email"]; ?></p>
 
-                            <p class="profile-label">Phone Number:</p>
+                            <label for="phone" class="profile-label">Phone Number:</label>
                             <p class="profile-value"><?php echo $userInfo["Phonenumber"]; ?></p>
-                            <a href="../index/homepage.php">back</a>
-                            <a href="../index/edit.php">edit</a>
+
+                            <a href="../index/homepage.php" class="button-link">&#128072; Back</a>
+                            <a href="../index/edit.php" class="button-link">&#9997; Edit</a>
                         </div>
                     </div>
                 </form>
