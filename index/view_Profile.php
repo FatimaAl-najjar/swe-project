c8fecf6461fbd632b58b091ab60362d582d1c8e7
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-10-24 16:56:04 +0300

    Revert "ana asef"
    This reverts commit 50fd9731b03920070f945fa9dc75297e99609d8b.

--- index/view_Profile.php
+++ index/view_Profile.php
@@ -2,13 +2,28 @@
 session_start();
 include '../includes/nav2.php';
 include_once "../includes/dbh.inc.php";
-?> 
+
+ ?>
 
 <head>
     <title>View Profile</title>
     <!-- <link rel="stylesheet" href="../css/viewProfile.css"> -->
 </head>
 <body>
+    <?php
+// Check if the user is logged in or signed up
+if (isset($_SESSION["ID"])) {
+    // User is logged in or signed up
+    $firstName = $_SESSION["firstName"];
+    $lastName = $_SESSION["lastName"];
+    $email = $_SESSION["Email"];
+    $password = $_SESSION["Password"];
+ 
+    // Redirect to the login or signup page
+    header("Location: view_Profile.php");
+    exit();
+}
+?> 
     <!-- Header  -->
     <h5>navbar here...</h5>
     <!-- Home  -->
@@ -22,13 +37,13 @@ include_once "../includes/dbh.inc.php";
                 <br><hr><br>
                 <div class="profile-info">
                     <p class="profile-label">User Name:</p>
-                    <p class="profile-value"><?php if (isset($_SESSION['ID'])){ echo $_SESSION["FirstName"] . " " . $_SESSION["LastName"]; }?></p>
+                    <p class="profile-value"><?php  echo $firstName . " " . $lastName; ?></p>
   
                     <p class="profile-label">Email:</p>
-                    <p class="profile-value"><?php  if (isset($_SESSION['ID'])){ echo $_SESSION["Email"];} ?></p>
+                    <p class="profile-value"><?php echo $email; ?></p>
                         
                     <p class="profile-label">Password:</p>
-                    <p class="profile-value"><?php if (isset($_SESSION['ID'])){ echo $_SESSION["Password"]; }?></p>
+                    <p class="profile-value"><?php echo $password; ?></p>
                 </div>
             </form>
         </div>
