ee4b50e02bb92598e3d734586d972abaf667f1df
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:14:14 +0300

    Working for now...

--- admin/view_patients.php
+++ admin/view_patients.php
@@ -12,4 +12,20 @@ if (!$result) {
     die('Error executing the query: ' . mysqli_error($conn));
 }
 
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+mysqli_free_result($result);
+
+foreach ($data as $row) {
+    echo "ID: " . $row['ID'] . "<br>";
+    echo "First Name: " . $row['FirstName'] . "<br>";
+    echo "Last Name: " . $row['LastName'] . "<br>";
+    echo "Email: " . $row['Email'] . "<br>";
+    echo "Phone number: " . $row['Phonenumber'] . "<br>";
+    echo "<br>";
+}
+
 ?>
