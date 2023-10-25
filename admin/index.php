e42135b34aff6cee27db65992f2fca1181b9b98b
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:38:07 +0300

    Number of patients in admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -8,3 +8,21 @@ include_once "../includes/dbh.inc.php";
 <h1 style="text-align: center">Admin dashboard</h1>
 <h5 style="text-align: center">For now Aked...</h5>
 
+<?php
+$query = "SELECT * FROM patients";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die('Error executing the query: ' . mysqli_error($conn));
+}
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+$rowCount = mysqli_num_rows($result);
+
+?>
+<h1>Statistics here..</h1>
+<h1>Number of patients: <?php echo "$rowCount"?></h1>
