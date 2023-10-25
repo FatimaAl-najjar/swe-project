fe73e720d1d055e003df56e657fa1101ad897fee
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:06:06 +0300

    Errors everywhere

--- admin/view_patients.php
+++ admin/view_patients.php
@@ -6,10 +6,10 @@ include_once "../includes/dbh.inc.php";
 
 
 $query = "SELECT * FROM patients";
-$result = mysqli_query($connection, $query);
+$result = mysqli_query($conn, $query);
 
 if (!$result) {
-    die('Error executing the query: ' . mysqli_error($connection));
+    die('Error executing the query: ' . mysqli_error($conn));
 }
 
 ?>
