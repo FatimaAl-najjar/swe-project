9e6964d570d064b9f0d0e0c6e87a94d4fc0b2f4c
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:21:19 +0300

    Simple table styling

--- admin/view_patients.php
+++ admin/view_patients.php
@@ -1,9 +1,7 @@
-<?php 
+<?php
 session_start();
 include '../includes/nav2.php';
 include_once "../includes/dbh.inc.php";
-
-
 
 $query = "SELECT * FROM patients";
 $result = mysqli_query($conn, $query);
@@ -12,20 +10,56 @@ if (!$result) {
     die('Error executing the query: ' . mysqli_error($conn));
 }
 
-
 $data = array();
 while ($row = mysqli_fetch_assoc($result)) {
     $data[] = $row;
 }
-mysqli_free_result($result);
 
-foreach ($data as $row) {
-    echo "ID: " . $row['ID'] . "<br>";
-    echo "First Name: " . $row['FirstName'] . "<br>";
-    echo "Last Name: " . $row['LastName'] . "<br>";
-    echo "Email: " . $row['Email'] . "<br>";
-    echo "Phone number: " . $row['Phonenumber'] . "<br>";
-    echo "<br>";
-}
+// mysqli_free_result($result);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patient List</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h1>Patient List</h1>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+        </tr>
+        <?php foreach ($data as $row): ?>
+            <tr>
+                <td><?php echo $row['ID']; ?></td>
+                <td><?php echo $row['FirstName']; ?></td>
+                <td><?php echo $row['LastName']; ?></td>
+                <td><?php echo $row['Email']; ?></td>
+                <td><?php echo $row['Phonenumber']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
