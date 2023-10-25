a6f9624ced71cc18ee4c3acb4e44a68843270a36
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:51:50 +0300

    Links

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@ include_once "../includes/dbh.inc.php";
 
 ?>
 
+
 <h1 style="text-align: center">Admin dashboard</h1>
 <h5 style="text-align: center">For now Aked...</h5>
 
@@ -21,3 +22,7 @@ $rowCount = mysqli_num_rows($result);
 ?>
 <h1>Statistics here..</h1>
 <h1>Number of patients: <?php echo "$rowCount"?></h1>
+
+<a href="announcements.php"><button type="button">Announcements</button></a>
+<br>
+<a href="view_patients.php"><button type="button">Patients data</button></a>
