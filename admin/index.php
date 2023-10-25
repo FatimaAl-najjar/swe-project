f3cab54cdc1a555167036a0855f734014c83bf23
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 20:32:44 +0300

    Admin dasboard page

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include '../includes/nav2.php';
+include_once "../includes/dbh.inc.php";
+
+?>
+
+<h1 style="text-align: center">Admin dashboard</h1>
+<h5 style="text-align: center">For now Aked...</h5>
+
