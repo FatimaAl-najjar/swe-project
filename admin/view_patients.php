c0d23545673cbbf6068e0beae977300d8516159c
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 19:54:43 +0300

    commit

--- admin/view_patients.php
+++ admin/view_patients.php
@@ -2,7 +2,14 @@
 session_start();
 include '../includes/nav2.php';
 include_once "../includes/dbh.inc.php";
+
+
+
+$query = "SELECT * FROM your_table";
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+    die('Error executing the query: ' . mysqli_error($connection));
+}
+
 ?>
-
-
-<h1>Habiba</h1>
