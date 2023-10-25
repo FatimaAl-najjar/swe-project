fb1fef21d1754c4e70a7cdcaaf6856cbf50e8f0c
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-10-25 21:21:35 +0300

    New table

--- admin/view_patients.php
+++ admin/view_patients.php
@@ -1,69 +1,126 @@
-<?php
-session_start();
-include '../includes/nav2.php';
-include_once "../includes/dbh.inc.php";
-
-$query = "SELECT * FROM patients";
-$result = mysqli_query($conn, $query);
-
-if (!$result) {
-    die('Error executing the query: ' . mysqli_error($conn));
-}
-
-$data = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
-}
-
-// mysqli_free_result($result);
-
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en" title="Coding design">
+
 <head>
-    <title style="text-align: center;">All patients</title>
-    <style>
-        title {
-            text-align: center;
-        }
-        
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View all patients</title>
+    <link rel="stylesheet" href="../css/admin_view.css">
 </head>
-<body>
-    <h1>Patient List</h1>
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-        </tr>
-        <?php foreach ($data as $row): ?>
-            <tr>
-                <td><?php echo $row['ID']; ?></td>
-                <td><?php echo $row['FirstName']; ?></td>
-                <td><?php echo $row['LastName']; ?></td>
-                <td><?php echo $row['Email']; ?></td>
-                <td><?php echo $row['Phonenumber']; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+<body>
+    <main class="table">
+        <section class="table__header">
+            <h1 style="text-align: center;">All patients</h1>
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                    <tr>
+                        <th> Id </th>
+                        <th> Customer </th>
+                        <th> Location </th>
+                        <th> Order Date </th>
+                        <th> Status </th>
+                        <th> Amount </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> 1 </td>
+                        <td> <img src="images/Zinzu Chan Lee.jpg" alt="">Zinzu Chan Lee</td>
+                        <td> Seoul </td>
+                        <td> 17 Dec, 2022 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong> $128.90 </strong></td>
+                    </tr>
+                    <tr>
+                        <td> 2 </td>
+                        <td><img src="images/Jeet Saru.jpg" alt=""> Jeet Saru </td>
+                        <td> Kathmandu </td>
+                        <td> 27 Aug, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$5350.50</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 3</td>
+                        <td><img src="images/Sonal Gharti.jpg" alt=""> Sonal Gharti </td>
+                        <td> Tokyo </td>
+                        <td> 14 Mar, 2023 </td>
+                        <td>
+                            <p class="status shipped">Shipped</p>
+                        </td>
+                        <td> <strong>$210.40</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 4</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Alson GC </td>
+                        <td> New Delhi </td>
+                        <td> 25 May, 2023 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 5</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Sarita Limbu </td>
+                        <td> Paris </td>
+                        <td> 23 Apr, 2023 </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 6</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Alex Gonley </td>
+                        <td> London </td>
+                        <td> 23 Apr, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 7</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Jeet Saru </td>
+                        <td> New York </td>
+                        <td> 20 May, 2023 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 8</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Aayat Ali Khan </td>
+                        <td> Islamabad </td>
+                        <td> 30 Feb, 2023 </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 9</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Alson GC </td>
+                        <td> Dhaka </td>
+                        <td> 22 Dec, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$249.99</strong> </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </main>
 </body>
+
 </html>
