80d91b2512e36fd89510d2621843b521106b2681
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-11-21 15:26:02 +0200

    COMMIT

--- admin/edit_patient.php
+++ admin/edit_patient.php
@@ -38,7 +38,7 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Edit Patient's data</title>
-        <link rel="stylesheet" href="static/css/edit_user.css">
+        <link rel="stylesheet" href="static/css/edit_patient.css">
     </head>
 
     <body>
