fa884fe5e8bc0f44d0efbd437160769c098aafb6
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-11-27 19:24:54 +0200

    function feedback

--- Classes/patient.php
+++ Classes/patient.php
@@ -6,7 +6,7 @@ $DB = "clinic";
 
 $conn = mysqli_connect($servername, $username, $password, $DB);
 
-if(!$conn) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
@@ -19,148 +19,103 @@ class Patient
     public $Password;
     public $Phonenumber;
 
-
-    function __construct($id)	{
-		if ($id !=""){
-			$sql="SELECT * FROM patients WHERE 	ID = $id";
-			$Patient = mysqli_query($GLOBALS['conn'], $sql);
-			if ($row = mysqli_fetch_array($Patient)){
-				$this->ID = $row["ID"];
-				$this->FirstName = $row["FirstName"];
-				$this->LastName = $row["LastName"];
-				$this->Email = $row["Email"];
-				$this->Password = $row["Password"];
-				$this->Phonenumber = $row["Phonenumber"];
-			}
-		}
-	}
-
-    static function find($id) {
-        $sql= "SELECT * FROM patients WHERE ID='$id'";
-        $result=mysqli_query($GLOBALS['conn'],$sql);
-		if ($row=mysqli_fetch_array($result)){
-			return new Patient($row[0]); 		
-		}
-		return NULL;
+    function __construct($id)
+    {
+        if ($id != "") {
+            $sql = "SELECT * FROM patients WHERE ID = $id";
+            $Patient = mysqli_query($GLOBALS['conn'], $sql);
+            if ($row = mysqli_fetch_array($Patient)) {
+                $this->ID = $row["ID"];
+                $this->FirstName = $row["FirstName"];
+                $this->LastName = $row["LastName"];
+                $this->Email = $row["Email"];
+                $this->Password = $row["Password"];
+                $this->Phonenumber = $row["Phonenumber"];
+            }
+        }
     }
-    static function login($FirstName, $LastName, $Password){
-		$sql="SELECT * FROM patients WHERE FirstName='$FirstName' AND LastName='$LastName' AND Password='$Password'";	
-		$result=mysqli_query($GLOBALS['conn'],$sql);
-		if ($row=mysqli_fetch_array($result)){
-			return new Patient($row[0]); 		
-		}
-		return NULL;
-	}
 
-    static function SelectAllPatients(){
-		$sql="select * from patients";
-		$Patients = mysqli_query($GLOBALS['conn'],$sql);
+    static function find($id)
+    {
+        $sql = "SELECT * FROM patients WHERE ID='$id'";
+        $result = mysqli_query($GLOBALS['conn'], $sql);
+        if ($row = mysqli_fetch_array($result)) {
+            return new Patient($row[0]);
+        }
+        return NULL;
+    }
+
+    static function login($FirstName, $LastName, $Password)
+    {
+        $sql = "SELECT * FROM patients WHERE FirstName='$FirstName' AND LastName='$LastName' AND Password='$Password'";
+        $result = mysqli_query($GLOBALS['conn'], $sql);
+        if ($row = mysqli_fetch_array($result)) {
+            return new Patient($row[0]);
+        }
+        return NULL;
+    }
+
+    static function SelectAllPatients()
+    {
+        $sql = "SELECT * FROM patients";
+        $Patients = mysqli_query($GLOBALS['conn'], $sql);
         $Result = [];
         $i = 0;
-		while ($row = mysqli_fetch_array($Patients)){
-			$MyObj= new Patient($row["ID"]);
-			$Result[$i]=$MyObj;
-			$i++;
-		}
-		return $Result;
-	}
+        while ($row = mysqli_fetch_array($Patients)) {
+            $MyObj = new Patient($row["ID"]);
+            $Result[$i] = $MyObj;
+            $i++;
+        }
+        return $Result;
+    }
 
-    static function addPatient($FirstName, $LastName, $Email, $Password, $Phonenumber)	{
-		$sql = "INSERT INTO patients (FirstName, LastName, Email, Password, Phonenumber) 
+    static function addPatient($FirstName, $LastName, $Email, $Password, $Phonenumber)
+    {
+        $sql = "INSERT INTO patients (FirstName, LastName, Email, Password, Phonenumber) 
         VALUES ('$FirstName', '$LastName', '$Email', '$Password', '$Phonenumber')";
-		if(mysqli_query($GLOBALS['conn'],$sql)) {
-            echo'Patient added succesfully!';
-			return true;
+        if (mysqli_query($GLOBALS['conn'], $sql)) {
+            echo 'Patient added successfully!';
+            return true;
+        } else {
+            echo 'Error adding patient: ' . mysqli_error($GLOBALS['conn']);
+            return false;
         }
-		else {
-            echo 'Error adding patient: ' . mysqli_error( $GLOBALS['conn'] );
-			return false;
-        }
-	}
+    }
 
-
-    function updatePatient(){
-		$sql = "UPDATE patients SET FirstName ='.$this->FirstName.' ,LastName='$this->LastName' ,Password='$this->Password' where ID = ".$this->ID;
-		if(mysqli_query($GLOBALS['conn'],$sql)) {
+    function updatePatient()
+    {
+        $sql = "UPDATE patients SET FirstName = '" . $this->FirstName . "', LastName = '$this->LastName', Password = '$this->Password' WHERE ID = " . $this->ID;
+        if (mysqli_query($GLOBALS['conn'], $sql)) {
             echo 'Patient updated successfully!';
-			return true;
+            return true;
+        } else {
+            echo 'Error updating patient: ' . mysqli_error($GLOBALS['conn']);
+            return false;
         }
-		else {
-            echo 'Error updating patient: '. mysqli_error( $GLOBALS['conn'] );
-			return false;
-        }	
-	}	
+    }
 
-
-    static function deletePatient($ObjPatient){
-		$sql = "DELETE FROM patients WHERE ID = " . $ObjPatient->ID;
-		if (mysqli_query($GLOBALS['conn'],$sql)) {
+    static function deletePatient($ObjPatient)
+    {
+        $sql = "DELETE FROM patients WHERE ID = " . $ObjPatient->ID;
+        if (mysqli_query($GLOBALS['conn'], $sql)) {
             echo 'Patient DELETED successfully!';
-			return true;
-        }        
-		else {
-            echo 'Error deleting patient: '. mysqli_error($GLOBALS['conn']);
-			return false;
+            return true;
+        } else {
+            echo 'Error deleting patient: ' . mysqli_error($GLOBALS['conn']);
+            return false;
         }
-	}
+    }
+
+    function addFeedback($feedback)
+    {
+        $sql = "INSERT INTO feedback (PatientID, Feedback) VALUES ('$this->ID', '$feedback')";
+        if (mysqli_query($GLOBALS['conn'], $sql)) {
+            echo 'Feedback added successfully!';
+            return true;
+        } else {
+            echo 'Error adding feedback: ' . mysqli_error($GLOBALS['conn']);
+            return false;
+        }
+    }
 }
-
-
-
-
-    // private $conn;
-    // public function __construct($conn) {
-    //     $this->conn = $conn;
-    // }
-
-    // public function addPatient($FirstName, $LastName, $Email, $Password, $Phonenumber) {
-    //     $FirstName = htmlspecialchars($FirstName);
-    //     $LastName = htmlspecialchars($LastName);
-    //     $Email = htmlspecialchars($Email);
-    //     $Password = htmlspecialchars($Password);
-    //     $Phonenumber = htmlspecialchars($Phonenumber);
-
-    //     $sql = "INSERT INTO http://localhost/swe-project/admin/view_patients.php(FirstName, LastName, Email, Password, Phonenumber) 
-    //             VALUES ('$FirstName', '$LastName', '$Email', '$Password', '$Phonenumber')";
-    //     $result = mysqli_query($this->conn, $sql);
-
-    //     if ($result) {
-    //         echo "Patient added successfully";
-    //     }
-    //     else {
-    //         echo "Error adding patient: " . mysqli_error($this->conn);
-    //     }
-    // }
-    
-    // public function UpdatePatient($FirstName, $LastName, $Email, $Password, $Phonenumber) {
-    //     $FirstName = htmlspecialchars($FirstName);
-    //     $LastName = htmlspecialchars($LastName);
-    //     $Email = htmlspecialchars($Email);
-    //     $Password = htmlspecialchars($Password);
-    //     $Phonenumber = htmlspecialchars($Phonenumber);
-    //     if($_SERVER["REQUEST_METHOD"]== "POST"){
-    //         $sql="UPDATE patients SET FirstName='$FirstName', LastName='$LastName', Email='$Email', Password='$Password',Phonenumber='$Phonenumber' 
-	//             WHERE id =".$_SESSION['id'];
-    //         $result = mysqli_query($this->conn, $sql);
-    //         if ($result) {
-    //             echo 'Patient updated successfully!';
-    //         }
-    //     }
-    // }
-
-    // public function deletePatient($FirstName, $LastName, $Email, $Password, $Phonenumber) {
-    //     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //         $id = $_SESSION['id'];
-            
-    //         $sql = "DELETE FROM patients WHERE id = $id";
-    //         $result = mysqli_query($this->conn, $sql);
-            
-    //         if ($result) {
-    //             echo 'Patient deleted successfully!';
-    //         } else {
-    //             echo 'Error deleting patient: ' . mysqli_error($this->conn);
-    //         }
-    //     }
-    // }
-
 ?>
