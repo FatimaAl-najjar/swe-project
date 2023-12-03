16a34a2b61442f96f53b37671cb1239105bb91a3
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-12-04 00:02:16 +0200

    PatientController

---
+++ MVC/app/controller/PatientController.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once(__ROOT__ . "controller/Controller.php");
+
+class PatientsController extends Controller{
+	public function insert() {
+		$FirstName = $_REQUEST['FirstName'];
+        $LastName = $_REQUEST['LastName'];
+        $Email = $_REQUEST['Email'];
+		$Password= $_REQUEST['Password'];
+		$Phonenumber = $_REQUEST['Phonenumber'];
+//validation
+		$this->model->insertPatient($FirstName,$LastName,$Email,$Password,$Phonenumber);
+	}
+
+	public function edit() {
+		$FirstName = $_REQUEST['FirstName'];
+        $LastName = $_REQUEST['LastName'];
+        $Email = $_REQUEST['Email'];
+		$Password= $_REQUEST['Password'];
+		$Phonenumber = $_REQUEST['Phonenumber'];
+		$this->model->editPatient($FirstName,$LastName,$Email,$Password,$Phonenumber);
+	}
+	
+	public function delete(){
+		$this->model->deletePatient();
+	}
+}
+?>
