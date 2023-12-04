5aef877d2836742f056893746b47d23e2c4807c0
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-12-04 23:53:13 +0200

    controller for the doctor's announcement

---
+++ MVC/app/controller/doctor'sAnnouncement.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once(__ROOT__ . "controller/Controller.php");
+
+class DoctorAnnouncementsController extends Controller {
+    public function insert() {
+        if (isset($_POST['submit'])) {
+            $announcement = $_POST['announcement'];
+            $dateAdded = date("Y-m-d H:i:s"); // Get the current date and time
+
+            if (empty(trim($announcement))) {
+                echo "Error: Announcement field is required";
+            } else {
+                $this->model->insertAnnouncement($announcement, $dateAdded);
+            }
+        }
+    }
+}
+?>
