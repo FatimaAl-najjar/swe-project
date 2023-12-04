c21d8bafe3abc89069eeae07e3e3c34761418ec7
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2023-12-04 23:55:20 +0200

    model for the doctor's announcements

---
+++ MVC/app/model/doctor'sAnnouncements.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(__ROOT__ . "model/Model.php");
+
+class DoctorAnnouncementsModel extends Model {
+    public function insertAnnouncement($announcement, $dateAdded) {
+        $sql = "INSERT INTO announcements (announcement, date_added) VALUES (?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ss", $announcement, $dateAdded);
+        $stmt->execute();
+        $stmt->close();
+    }
+}
+?>
