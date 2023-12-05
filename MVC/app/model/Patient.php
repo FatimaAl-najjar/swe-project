ca7ad440c4d00048ec7525924b3d4d8e96f1be5a
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-12-05 23:55:55 +0200

    push l menna

--- MVC/app/model/Patient.php
+++ MVC/app/model/Patient.php
@@ -21,7 +21,7 @@ class Patient extends Model {
       $this->FirstName = $FirstName;
       $this->LastName = $LastName;
       $this->Email=$Email;
-	  $this->Password=$Password;
+	    $this->Password=$Password;
       $this->Phonenumber = $Phonenumber;
     }
   }
