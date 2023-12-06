259991ee14900bf5317e6488ddf74b0882adb86b
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-12-06 21:45:35 +0200

    just trying

---
+++ MVC/public/feedbackpatient.php
@@ -0,0 +1,16 @@
+<?php
+
+define('__ROOT__', "../app/");
+
+// Include necessary files
+require_once(__ROOT__ . "model/Feedback.php");
+require_once(__ROOT__ . "controller/FeedbackController.php");
+require_once(__ROOT__ . "view/ViewFeedback.php");
+
+// Create instances of the model, controller, and view
+$model = new Feedback("0");
+$controller = new FeedbackController($model);
+$view = new ViewFeedback($controller, $model);
+
+echo $view->outputFeedback();
+?>
