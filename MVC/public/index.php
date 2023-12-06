88db6a7a8cb841a081387eac3401c322abc4ba01
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-06 16:14:59 +0200

    Patient controller

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="css/home.css">
 <?php
 define('__ROOT__', "../app/");
-require_once(__ROOT__ . "model/Patients.php");
-require_once(__ROOT__ . "controller/PatientController.php");
-require_once(__ROOT__ . "view/ViewPatient.php");
+// require_once(__ROOT__ . "model/Patients.php");
+// require_once(__ROOT__ . "controller/PatientController.php");
+// require_once(__ROOT__ . "view/ViewPatient.php");
 
-$model = new Patients();
-$controller = new PatientController($model);
-$view = new ViewPatient($controller, $model);
+// $model = new Patients();
+// $controller = new PatientController($model);
+// $view = new ViewPatient($controller, $model);
 
 if (isset($_GET['action']) && !empty($_GET['action'])) {
 	$controller->{$_GET['action']}();
@@ -157,4 +157,4 @@ if(isset($_POST['login']))	{
         </script> 
         <link rel="stylesheet" href="css/index.css">
 
-<?php echo $view->loginForm();?>
+<?php //echo $view->loginForm();?>
