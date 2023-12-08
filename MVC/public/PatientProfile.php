9ab2a3917f709b1acb7eb7da6e26999d25f7678d
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-08 23:12:52 +0200

    __ROOT__ error

--- MVC/public/PatientProfile.php
+++ MVC/public/PatientProfile.php
@@ -1,8 +1,10 @@
 <?php 
 
+define('__ROOT__', "../app/");
 require_once(__ROOT__ . "model/Patient.php");
 require_once(__ROOT__ . "controller/PatientController.php");
 require_once(__ROOT__ . "view/ViewPatient.php");
+
 ?>
 
 <?php
