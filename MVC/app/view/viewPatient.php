c6c1c67800a63853b9ffd2d9cfacd63c51c64024
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-08 22:36:30 +0200

    css

--- MVC/app/view/viewPatient.php
+++ MVC/app/view/viewPatient.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/PatientProfile.css">
 	</head>
 </html>
 
@@ -49,18 +50,36 @@ class ViewPatient extends View{
 	}
 	
 	function loginForm(){
-		$str='<div class="card">
-		<h1>Login</h1>
-		<form action="try.php" method="POST">
-			<label>Email:</label>
-			<input type="text" placeholder="please enter your e-mail" name="Email" required><br>
-			<label>Password:</label>
-			<input type="password" placeholder="please enter your password" name="Password" required><br>
-			<button class="btn" type="submit" name="submit" value="submit">Login</button>
-			<a href="signup.php"><button type="button" class="btn">Sign up</button></a>     
-			<a href="homepage.php"><button type="button" class="btn">Back</button></a>     
-			</form>
-		</div>';
+		$str='
+		<section class="home">
+			<div class="form-container">
+				<i class="uil uil-times form_close"></i>
+				<div class="form view_form">
+					<form action="#">
+						<div class="card1">
+							<h2 style="text-align: center;">My Profile</h2>
+							<br><hr><br>
+							<div class="profile-info">
+								$str="";
+								<label for="username" class="profile-label">User Name:</label>';
+								$str .= '<p class="profile-value">Welcome ' . $this->model->getFName() . '</p>';
+
+								$str .= '<label for="email" class="profile-label">Email:</label>';
+								$str .= '<p class="profile-value">LastName ' . $this->model->getLName() . '</p>';
+
+
+								$str .= '<label for="phone" class="profile-label">Phone Number:</label>';
+								$str .= '<p class="profile-value">phone: ' . $this->model->getPhonenumber() . '</p>';
+
+								$str .= '<a href="PatientProfile.php?action=edit" class="button-link">Edit Profile</a><br><br>';
+								$str .= '<a href="PatientProfile.php?action=signOut" class="button-link">Sign Out</a><br><br>';
+								$str .= '<a href="PatientProfile.php?action=delete" class="button-link">Delete Account</a>';
+							$str .= '</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>';
 		return $str;
 	}
 
