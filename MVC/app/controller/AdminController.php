954f465c93141b71479cdc1cda960fdfe87e6b45
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2023-12-08 15:46:17 +0200

    ya gma3a function login mfish validation

--- MVC/app/controller/AdminController.php
+++ MVC/app/controller/AdminController.php
@@ -38,15 +38,31 @@ class AdminController extends Controller{
 	public function deleteAdmin(){
 		$this->model->deleteadmin();
 	}
-	public function adminlogin()
+	public function adminlogin($Email, $Username, $Password, $db)
     {
+		$stmt =$this->db->prepare("SELECT * FROM admins WHERE Email =? AND Username=? AND Password=?");
+		$stmt->bimd_param('sss',$Email,$Username,$Password);
+		$stmt->execute();
         
-        $Email = $_REQUEST['Email'];
+		$result= $stmt->get_result()->fetch_assoc();
+
+		if($result !=null){
+			$_SESSION['Id']=$result['Id'];
+			$_SESSION['Email']=$result['Email'];
+			$_SESSION['Username']=$result['Username'];
+			$_SESSION['Password']=$result['Password'];
+
+	    /* $Email = $_REQUEST['Email'];
 		$Username=$_REQUEST['Username'];
         $Password = $_REQUEST['Password'];
         $this->model->login( $Email,$Username ,$Password);
-        
-    }
+        */
+    }else{
+		echo"invalid admin data";
+	}
+
+}
+
 
 }
 ?>
--- MVC/public/adminLogin.php
+++ MVC/public/adminLogin.php
@@ -5,15 +5,18 @@ require_once(__ROOT__ . "controller/AdminController.php");
 require_once(__ROOT__ . "view/ViewAdmin.php");
 
 $model = new Admin("");
+$db = new Dbh();
 $controller = new AdminController($model);
 $view = new ViewAdmin($controller, $model);
 
 if (isset($_GET['action']) && !empty($_GET['action'])) {
 	$controller->{$_GET['action']}();
 
-if ($action == 'like') {
+if ($action == '') {
         
-    $controller->adminlogin();
+    $controller->adminLogin($_REQUEST['Email'], $_REQUEST['Username'], $_REQUEST['Password']);
+    header("Location: ..admin/edit_patient.php");
+    exit();
 
 }
 }
