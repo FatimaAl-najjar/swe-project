f3367a198e2acd5b386ab2ecda5598c23044bfff
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-08 16:13:42 +0200

    CSS linking

--- MVC/app/view/viewPatient.php
+++ MVC/app/view/viewPatient.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/home.css">
+	</head>
+</html>
+
 <?php
 
 require_once(__ROOT__ . "view/View.php");
