c6784b7f28b5a4ababd7316bb6b1dab31896b3e9
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-12-09 12:17:16 +0200

    f error 3la el fetch_assoc

--- MVC/public/feedbackpatient.php
+++ MVC/public/feedbackpatient.php
@@ -3,14 +3,34 @@
 define('__ROOT__', "../app/");
 
 // Include necessary files
-require_once(__ROOT__ . "model/Feedback.php");
+require_once(__ROOT__ . "model/Feedbacks.php");
 require_once(__ROOT__ . "controller/FeedbackController.php");
 require_once(__ROOT__ . "view/ViewFeedback.php");
 
 // Create instances of the model, controller, and view
-$model = new Feedback("0");
+$model = new Feedbacks();
 $controller = new FeedbackController($model);
-$view = new FeedbackView($controller, $model);
+$view = new ViewFeedback($controller, $model);
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+	switch($_GET['action']){
+		case 'insert':
+			$controller->insertFeedback();
+			echo $view->output();
+			break;
+		case 'edit':
+			echo $view->editFeedback($_GET['id']);
+			break;
+		case 'editAction':
+			$controller->edit($_GET['id']);
+			echo $view->output();
+			break;
+		case 'delete':
+			$controller->deleteFeedback($_GET['id']);
+			echo $view->output();
+			break;
+	}
+}
+else
+	echo $view->output();
 
-echo $view->insertFeedback();
 ?>
