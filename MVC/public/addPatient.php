8dcaf8561c9369d77ae30cecc8988b044ef6f65b
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-19 22:39:43 +0200

    add patient validation

--- MVC/public/addPatient.php
+++ MVC/public/addPatient.php
@@ -3,6 +3,9 @@ define('__ROOT__', "../app/");
 require_once(__ROOT__ . "model/Admin.php");
 require_once(__ROOT__ . "controller/AdminController.php");
 require_once(__ROOT__ . "view/ViewAdmin.php");
+require_once(__ROOT__ . "model/Patients.php");
+
+$model2 = new Patients();
 
 $model = new Admin("");
 $db = new Dbh();
@@ -20,8 +23,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         $Password = $_POST['Password'];
         $Phonenumber = $_POST['Phonenumber'];
 
-        $controller->addPatient($FirstName, $LastName, $Email, $Password, $Phonenumber);
-    }else{
+        $existingPatient = $model2->getPatientByEmail($Email);
+        if (empty($FirstName) || empty($LastName)) {
+            echo "Error: First Name and Last Name cannot be empty." . "<br>";
+        }
+        if ($existingPatient) {
+            // Patient with the same email already exists
+            echo "Error: Patient with the same email already exists." . "<br>";
+        }
+        if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+            echo "Error: Invalid email format." . "<br>";
+        }
+        if (!preg_match('/^\d{11}$/', $Phonenumber)) {
+            echo "Error: Invalid phone number format (should be 11 digits)." . "<br>";
+        }
+        else {
+            $controller->addPatient($FirstName, $LastName, $Email, $Password, $Phonenumber);
+        }
+    }
+    else{
         $errorMessage = "<h2>An error occurred during adding admin.</h2>";
     }
     echo $errorMessage;
