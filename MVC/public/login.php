f3897fc89b1614e4d453883951560b2bdbd3e527
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2023-12-19 10:54:18 +0200

    Homepage for patient

--- MVC/public/login.php
+++ MVC/public/login.php
@@ -2,15 +2,38 @@
 <?php
 define('__ROOT__', "../app/");
 require_once(__ROOT__ . "model/Patient.php");
+require_once(__ROOT__ . "model/Patients.php");
 require_once(__ROOT__ . "controller/PatientController.php");
 require_once(__ROOT__ . "view/ViewPatient.php");
 
-$model = new Patient(123);
+$model = new Patients();
 $controller = new PatientController($model);
 $view = new ViewPatient($controller, $model);
 
+// $model = new Patient(123);
+// $controller = new PatientController($model);
+// $view = new ViewPatient($controller, $model);
+
 if (isset($_GET['action']) && !empty($_GET['action'])) {
 	$controller->{$_GET['action']}();
+}
+
+if(isset($_POST['submit']))	{
+	$Email = $_REQUEST["Email"];
+	$Password = $_REQUEST["Password"];
+	$sql = "SELECT * FROM patients where Email='$Email' AND Password='$Password'";
+	$dbh = new Dbh();
+	$result = $dbh->query($sql);
+	if ($result->num_rows == 1){
+		$row = $dbh->fetchRow();
+		$_SESSION["ID"]=$row["ID"];
+		$_SESSION["Email"]=$row["Email"];
+		echo $_SESSION["ID"];
+		echo $_SESSION["Email"];
+		header("Location:index.php");
+		// header("Location:feedbackPatient.php");
+		// header("Location:PatientProfile.php"); // WORKING
+	}
 }
 
 ?>
